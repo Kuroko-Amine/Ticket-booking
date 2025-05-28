a62c48213b9e1085a9a2a55db3f0600e05bdc3a7
Mohamed Amine Cherfaoui <153374397+Kuroko-Amine@users.noreply.github.com>
2025-05-28 02:53:06 +0100

    Delete Front-end directory

--- Front-end/Js/terms.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const header = document.getElementById('stickyHeader');
-    if (window.scrollY > 100) { // Show header after scrolling 100px
-        header.classList.add('visible');
-    } else {
-        header.classList.remove('visible');
-    }
-});
